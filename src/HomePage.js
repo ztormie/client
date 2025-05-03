6a833d9df07fa149bb632d09ebb511e95a52a1c3
ztormie <72881908+ztormie@users.noreply.github.com>
2025-05-03 16:31:20 +0300

    fixa framsida

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,7 +8,7 @@ export default function HomePage() {
     <div className="min-h-screen bg-[#fffaf4] flex flex-col items-center px-6 pt-10 text-center">
 
       {/* Header */}
-      <h1 className="text-3xl md:text-4xl font-extrabold text-gray-800 mb-1">
+      <h1 className="text-3xl md:text-4xl font-extrabold text-gray-800 mb-6">
         Hjälpsamma Tjänster <span>🐶</span>
       </h1>
 
@@ -21,21 +21,21 @@ export default function HomePage() {
 
       {/* Service buttons */}
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-12 mb-12 w-full max-w-2xl">
-        <div className="bg-red-50 shadow-md rounded-xl p-4 h-60 flex flex-col items-center justify-center">
+        <div className="bg-red-50 shadow-md rounded-xl p-4 h-60 flex flex-col items-center justify-center transform transition duration-300 hover:-translate-y-1 hover:shadow-xl">
           <span className="text-3xl mb-2">🐾</span>
           <Link to="/bokning/hund" className="bg-yellow-200 hover:bg-blue-200 text-orange-900 font-semibold py-2 px-4 rounded-xl text-center">
             Hundpassning
           </Link>
         </div>
 
-        <div className="bg-red-50 shadow-md rounded-xl p-4 h-60 flex flex-col items-center justify-center">
+        <div className="bg-red-50 shadow-md rounded-xl p-4 h-60 flex flex-col items-center justify-center transform transition duration-300 hover:-translate-y-1 hover:shadow-xl">
           <span className="text-3xl mb-2">👶</span>
           <Link to="/bokning/barn" className="bg-yellow-200 hover:bg-blue-200 text-orange-900 font-semibold py-2 px-4 rounded-xl">
             Barnpassning
           </Link>
         </div>
 
-        <div className="bg-red-50 shadow-md rounded-xl p-4 h-60 flex flex-col items-center justify-center">
+        <div className="bg-red-50 shadow-md rounded-xl p-4 h-60 flex flex-col items-center justify-center transform transition duration-300 hover:-translate-y-1 hover:shadow-xl">
           <span className="text-3xl mb-2">🛒</span>
           <Link to="/bokning/arenden" className="bg-yellow-200 hover:bg-blue-200 text-amber-900 font-semibold py-2 px-4 rounded-xl">
             Hjälp med ärenden
